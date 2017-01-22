5015013f8c351f78b02129483be89ecff94da838
rvrvrv <ibanez19@sbcglobal.net>
2017-01-22 12:29:40 -0600

    Update formatting on HTML and JS docs

--- 01-drumkit/js/index.js
+++ 01-drumkit/js/index.js
@@ -1,30 +1,31 @@
-$(document).ready(function() {
-  //Pure JS for keyboard events
-  window.addEventListener('keydown', playSound);
-
-  function playSound(e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`); //Grab mp3 link from audio tag
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`); //Grab correct key to animate
-    if (!audio) return; //If no audio w/associated keypress, exit
-    key.classList.add('playing'); //Light the button
-    audio.currentTime = 0; //Rewind to start of sample
-    audio.play(); //Play the sound
-    setTimeout(function() {
-      key.classList.remove('playing');
-    }, 150); //Prevent button from staying lit
-  }
-
-  //jQuery for clicks
-  $('.key').click(playSound2);
-
-  function playSound2() {
-    const btn = $(this);
-    btn.addClass('playing');
-    const audio = document.querySelector('audio[data-key="' + btn.attr('data-key') + '"]');
-    audio.currentTime = 0; //Rewind to start of sample
-    audio.play(); //Play the sound
-    setTimeout(function() {
-      btn.removeClass('playing');
-    }, 150);
-  }
+/*jshint browser: true, esversion: 6*/
+/* global $*/
+$(document).ready(function () {
+	//Play sound based on keyboard event, without jQuery
+	function playSound(e) {
+		const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`); //Grab mp3 link from audio tag
+		const key = document.querySelector(`.key[data-key="${e.keyCode}"]`); //Grab correct key to animate
+		if (!audio) return; //If no audio w/associated keypress, exit
+		key.classList.add('playing'); //Light the button
+		audio.currentTime = 0; //Rewind to start of sample
+		audio.play(); //Play the sound
+		setTimeout(function () {
+			key.classList.remove('playing');
+		}, 150); //Prevent button from staying lit
+	}
+	//Play sound based on mouse event, using jQuery
+	function playSound2() {
+		const btn = $(this);
+		btn.addClass('playing');
+		const audio = document.querySelector('audio[data-key="' + btn.attr('data-key') + '"]');
+		audio.currentTime = 0; //Rewind to start of sample
+		audio.play(); //Play the sound
+		setTimeout(function () {
+			btn.removeClass('playing');
+		}, 150);
+	}
+	//Pure JS for keyboard events
+	window.addEventListener('keydown', playSound);
+	//jQuery for clicks
+	$('.key').click(playSound2);
 });
