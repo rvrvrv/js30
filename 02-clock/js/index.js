a362578bab8e0e8bdb4dba112859826fde727d1c
rvrvrv <ibanez19@sbcglobal.net>
2017-01-23 10:29:01 -0600

    Update styling and fix 90-degree jitter

--- 02-clock/js/index.js
+++ 02-clock/js/index.js
@@ -1,7 +1,6 @@
 /*jshint browser: true, esversion: 6*/
 /* global $*/
 //$(document).ready(function () {
-const allHands = document.querySelector('.hand');
 const secondHand = document.querySelector('.second-hand');
 const minuteHand = document.querySelector('.minute-hand');
 const hourHand = document.querySelector('.hour-hand');
@@ -17,10 +16,11 @@ function setDate() {
 	const minDeg = ((min / 60) * 360) + ((sec / 60) * 6) + 90;
 	const hrDeg = ((hr / 12) * 360) + ((min / 60) * 30) + 90;
 	if (secDeg == 90) { //Prevent jittery hand reset
-		allHands.style.transition = 'none';
+		secondHand.style.transition = 'none';
+		secondHand.style.transform = `rotate(${secDeg}deg)`;
 	}
 	else {
-		allHands.style.transition = 'all 0.05s';
+		secondHand.style.transition = 'all 0.05s';
 	}
 	//Rotation operations
 	secondHand.style.transform = `rotate(${secDeg}deg)`;
