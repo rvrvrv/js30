f42552e2a2ea369daedf3b3b7cb0017dc1f60d18
rvrvrv <ibanez19@sbcglobal.net>
2017-04-09 12:18:11 -0500

    Initial upload

---
+++ 09-devtools/devtools.js
@@ -0,0 +1,32 @@
+const dogs = [{ name: 'Snickers', age: 2 }, { name: 'hugo', age: 8 }];
+
+function makeGreen() {
+	const p = document.querySelector('p');
+	p.style.color = '#0b3';
+	p.style.fontSize = '48px';
+}
+
+// Regular
+
+// Interpolated
+
+// Styled
+
+// warning!
+
+// Error :|
+
+// Info
+
+// Testing
+
+// clearing
+
+// Viewing DOM Elements
+
+// Grouping together
+
+// counting
+
+// timing
+
