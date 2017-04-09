f8ebb39d2b5e094463bf2181af1e15ec1a1a0d4e
rvrvrv <ibanez19@sbcglobal.net>
2017-04-09 15:54:34 -0500

    Upload completed files

---
+++ 10-shiftcheck/js/index.js
@@ -0,0 +1,22 @@
+const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+let lastChecked;
+
+function handleCheck(e) {
+	let inBetween = false;
+
+	if (e.shiftKey && this.checked) {
+		checkboxes.forEach(checkbox => {
+
+			if (checkbox === this || checkbox === lastChecked) {
+				inBetween = !inBetween;
+			}
+			//Check all boxes in between currently and previously clicked
+			if (inBetween) checkbox.checked = true;
+
+		});
+	}
+	lastChecked = this;
+}
+
+//Listen for checks on all boxes
+checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
