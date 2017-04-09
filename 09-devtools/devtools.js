9dd7cf8ed2cb445c2e23d3a7e015037e9620fec0
rvrvrv <ibanez19@sbcglobal.net>
2017-04-09 12:45:13 -0500

    Upload completed JS

--- 09-devtools/devtools.js
+++ 09-devtools/devtools.js
@@ -1,32 +1,72 @@
-const dogs = [{ name: 'Snickers', age: 2 }, { name: 'hugo', age: 8 }];
+const dogs = [
+	{ name: 'Snickers', age: 2 },
+	{ name: 'Hugo', age: 8 },
+	{ name: 'Rambo', age: 4 }
+];
 
 function makeGreen() {
 	const p = document.querySelector('p');
-	p.style.color = '#0b3';
+	p.style.color = '#0a4';
 	p.style.fontSize = '48px';
 }
 
 // Regular
+console.log('Hello');
 
 // Interpolated
+console.log('Hello, %s', 'John Doe');
+
+let name = 'Jane Smith';
+console.log(`Hello, ${name}`);
 
 // Styled
+console.log('%c Styled log ', 'background:#050; color: #fff; font-weight:900; font-size:2em;');
 
-// warning!
+// Warning!
+console.warn('Potential issue');
 
 // Error :|
+console.error('Error!');
 
 // Info
+console.info('Fun fact here');
 
 // Testing
+setTimeout(() => {
+	const p = document.querySelector('p');
+	console.assert(p.style.color === '', 'Not black');
+}, 3000);
 
-// clearing
+// Clearing
+setTimeout(() => {
+	console.clear();
+}, 20000);
 
 // Viewing DOM Elements
+const p = document.querySelector('p');
+console.log(p);
+console.dir(p);
 
 // Grouping together
+console.group('dogs');
+dogs.forEach(dog => {
+	console.groupCollapsed(`${dog.name}`);
+	console.log(`${dog.name} is ${dog.age} years old.`);
+	console.log(`${dog.name} is ${dog.age * 7} in dog years!`);
+	console.groupEnd(`${dog.name}`);
+});
+console.groupEnd('dogs');
 
-// counting
+// Counting
+console.count('Mike');
+console.count('Frank');
+console.count('Mike');
 
-// timing
-
+// Timing
+console.time('fetch');
+fetch('https://api.github.com/users/rvrvrv')
+	.then(data => data.json())
+	.then(data => {
+		console.timeEnd('fetch');
+		console.log(data);
+	});
