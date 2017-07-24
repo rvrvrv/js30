793eba799a05e0256351f1ced99dd16396898800
rvrvrv <ibanez19@sbcglobal.net>
2017-07-24 12:20:57 -0500

    Upload completed files

---
+++ 12-keysequence/main.js
@@ -0,0 +1,17 @@
+/*jshint browser: true, esversion: 6*/
+/* global $, console, cornify_add */
+
+const pressed = [];
+const secretCode = 'rainbow';
+
+window.addEventListener('keyup', e => {
+	//Log last key pressed
+	pressed.push(e.key);
+	//Trim pressed array to correct code length
+	pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
+	//Check for matching code
+	if (pressed.join('').includes(secretCode)) {
+		console.log('Matched!');
+		cornify_add(); //Display surprise via cornify.js
+	}
+});
