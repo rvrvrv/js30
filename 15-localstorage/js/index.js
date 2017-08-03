43d252fdbd54d14380f67055f149eef67ee4e034
rvrvrv <ibanez19@sbcglobal.net>
2017-08-03 12:58:28 -0500

    Reorder functions

--- 15-localstorage/js/index.js
+++ 15-localstorage/js/index.js
@@ -3,77 +3,82 @@ const allBtns = document.querySelectorAll('.all-btn');
 const itemsList = document.querySelector('.plates');
 const items = JSON.parse(localStorage.getItem('items')) || [];
 
-//Add new item to items array
-function addItem(e) {
-    e.preventDefault();
-    //Get and store user's item
-    const text = this.querySelector('[name=item]').value;
-    const item = {
-        text, //Shorthand to store 'const text' as 'item.text'
-        done: false
-    };
-    //Clear add-item field
-    this.reset();
-    //Add user's item to overall array
-    items.push(item);
-    //Generate HTML
-    populateList(items, itemsList);
-    //Store list in localStorage
-    localStorage.setItem('items', JSON.stringify(items));
-}
-
-//Generate HTML for list of items
+// Generate HTML for list of items
 function populateList(plates = [], platesList) {
-    platesList.innerHTML = plates
-        .map((plate, i) => {
-            return `
+  platesList.innerHTML = plates
+    .map(
+      (plate, i) => `
             <li>
-                <input type='checkbox' data-index=${i} id='item-${i}'
+                <input type='checkbox' data-index=${i} id="item-${i}"
                     ${plate.done ? 'checked' : ''} />
                 <label for='item-${i}'>${plate.text}</label>
             </li>
-            `;
-        })
-        .join(''); //Return mapped array as string (HTML)
+            `,
+    )
+    .join(''); // Return mapped array as string (HTML)
 }
 
-//Change item's done (checked) status
+// Add new item to items array
+function addItem(e) {
+  e.preventDefault();
+  // Get and store user's item
+  const text = this.querySelector('[name=item]').value;
+  const item = {
+    text, // Shorthand to store 'const text' as 'item.text'
+    done: false,
+  };
+  // Clear add-item field
+  this.reset();
+  // Add user's item to overall array
+  items.push(item);
+  // Generate HTML
+  populateList(items, itemsList);
+  // Store list in localStorage
+  localStorage.setItem('items', JSON.stringify(items));
+}
+
+// Change item's done (checked) status
 function toggleDone(e) {
-    if (!e.target.matches('input')) return; //Ignore non-checkbox clicks
-    //Get index of clicked item
-    const index = e.target.dataset.index;
-    //Toggle done property of clicked item
-    items[index].done = !items[index].done;
-    //Update localStorage and list
+  if (!e.target.matches('input')) {
+    return;
+  } // Ignore non-checkbox clicks
+  // Get index of clicked item
+  const index = e.target.dataset.index;
+  // Toggle done property of clicked item
+  items[index].done = !items[index].done;
+  // Update localStorage and list
+  localStorage.setItem('items', JSON.stringify(items));
+  populateList(items, itemsList);
+}
+
+// Clear, check, or uncheck all items
+function affectAll(e) {
+  const task = e.target.dataset.task;
+  // Clear all items
+  if (task === 'clear') {
+    if (confirm('Are you sure you want to clear all of the items?')) {
+      localStorage.removeItem('items');
+      items.length = 0;
+      itemsList.innerHTML = '';
+    }
+  }
+  // Check or uncheck all items
+  if (task.includes('check')) {
+    items.forEach(item => {
+      if (task === 'check') {
+        item.done = true;
+      } else {
+        item.done = false;
+      }
+    });
     localStorage.setItem('items', JSON.stringify(items));
     populateList(items, itemsList);
-}
-
-//Clear, check, or uncheck all items
-function affectAll(e) {
-    const task = e.target.dataset.task;
-    //Clear all items
-    if (task === 'clear') {
-        if (confirm('Are you sure you want to clear all of the items?')) {
-            localStorage.removeItem('items');
-            items.length = 0;
-            itemsList.innerHTML = '';
-        }
-    }
-    //Check or uncheck all items
-    if (task.includes('check')) {
-        items.forEach(item => {
-            if (task === 'check') item.done = true;
-            else item.done = false;
-        });
-        localStorage.setItem('items', JSON.stringify(items));
-        populateList(items, itemsList);
-    }
+  }
 }
 
 addItems.addEventListener('submit', addItem);
 itemsList.addEventListener('click', toggleDone);
 allBtns.forEach(btn => btn.addEventListener('click', affectAll));
 
-//Upon page load, populate list from localStorage
+// Upon page load, populate list from localStorage
 populateList(items, itemsList);
