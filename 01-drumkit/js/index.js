7b9fa6a81bb3df5f84802e5c80586c74acc9f699
rvrvrv <ibanez19@sbcglobal.net>
2017-08-07 11:21:21 -0500

    Implement vanilla-JS click event handler

--- 01-drumkit/js/index.js
+++ 01-drumkit/js/index.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   // Play sound based on keyboard event
-  function playSound(e) {
+  function playSound(e, clicked) {
+    if (!e.keyCode) e.keyCode = clicked;
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`); // Grab mp3 link from audio tag
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`); // Grab correct key to animate
     if (!audio) return; // If no audio w/associated keypress, exit
@@ -9,18 +10,21 @@ document.addEventListener('DOMContentLoaded', () => {
     audio.play(); // Play the sound
     setTimeout(() => key.classList.remove('playing'), 150); // Prevent button from staying lit
   }
-  // Play sound based on mouse event, using jQuery
-  function playSound2() {
-    const btn = $(this);
-    btn.addClass('playing');
-    const audio = document.querySelector(`audio[data-key="${btn.attr('data-key')}"]`);
-    audio.currentTime = 0; // Rewind to start of sample
-    audio.play(); // Play the sound
-    setTimeout(() => {
-      btn.removeClass('playing');
-    }, 150);
+  // Trigger playSound from button click
+  function handleClick(e) {
+    // Always select the button div, not an inner element
+    const btn = (e.target.tagName === 'DIV')
+      ? e.target
+      : e.target.parentElement;
+    // Call playSound with correct keycode
+    playSound(e, btn.getAttribute('data-key'));
   }
 
-  window.addEventListener('keydown', playSound); // Listen for keypress
-  $('.key').click(playSound2); // Listen for clicks
+  // Listen for keypress
+  window.addEventListener('keydown', playSound);
+  // Listen for clicks
+  const btns = document.getElementsByClassName('key');
+  for (let i = 0; i < btns.length; i++) {
+    btns[i].addEventListener('click', handleClick);
+  }
 });
