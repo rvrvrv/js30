bc21a6fbb8cdaf4403e4af370f74b1ea8cf02a90
rvrvrv <ibanez19@sbcglobal.net>
2017-10-29 12:54:32 -0500

    Display location

--- 21-geolocation/js/index.js
+++ 21-geolocation/js/index.js
@@ -1,14 +1,18 @@
 const arrow = document.getElementsByClassName('arrow')[0];
 const speed = document.getElementsByClassName('speed')[0];
+const loc = document.getElementsByClassName('location')[0];
 
 // Monitor accelerometer data
 navigator.geolocation.watchPosition((d) => {
-  // Display speed
-  speed.textContent = d.coords.speed ? `${d.coords.speed} km/h` : '';
   // Point compass
   arrow.style.transform = `rotate(${d.coords.heading}deg)`;
+  // Display speed
+  speed.textContent = d.coords.speed ? `${d.coords.speed} km/h` : '';
+  // Display location
+  loc.innerHTML = `Current Location:<br>${d.coords.latitude.toFixed(4)}, ${d.coords.longitude.toFixed(4)}`;
 }, (err) => {
   // If error, notify user
-  document.body.innerHTML = `<h3>The following error has occurred:<br><br>${err.message}</h3>`;
+  document.body.innerHTML = '<h3>An error has occurred.</h3>';
+  if (err.message) document.body.innerHTML += `<h3 style="color:#faa;">${err.message}</h3>`;
 });
 
