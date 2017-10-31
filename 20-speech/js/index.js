7a6cd18eed99939bf9187288f0d121426d57077b
rvrvrv <ibanez19@sbcglobal.net>
2017-10-31 09:37:55 -0500

    Implement toggle functionality

--- 20-speech/js/index.js
+++ 20-speech/js/index.js
@@ -1,47 +1,74 @@
 // Get notebook div on page
 const notebook = document.getElementsByClassName('notebook')[0];
-
-// Initialize with maximum browser compatibility
-window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-const recognition = new SpeechRecognition();
-
-// Capture results during speech
-recognition.interimResults = true;
-
+// Get button on page
+const btn = document.getElementsByTagName('button')[0];
 // Create paragraph element to display captured speech
 let p = document.createElement('p');
 notebook.appendChild(p);
+// Initialize recognition with maximum browser compatibility
+window.SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
 
-// Listen to and process results
-recognition.addEventListener('result', (e) => {
-  // First, convert array-like results to array
-  const transcript = Array.from(e.results)
-    // Store current speech
-    .map(result => result[0])
-    // Store transcript
-    .map(result => result.transcript)
-    // Combine both results into one string
-    .join('');
+// Change the background to a random color
+function randomBgColor() {
+  document.body.style.backgroundColor = `hsl(${Math.floor(
+    Math.random() * 360
+  )}, 100%, 20%)`;
+}
 
-  // Display transcript in notebook paragraph
-  p.textContent = transcript;
+// Capture and display speech
+function captureSpeech() {
+  // Capture results during speech
+  recognition.interimResults = true;
 
-  // At end of detected speech, create a new paragraph and change the background color
-  if (e.results[0].isFinal) {
-    p = document.createElement('p');
-    notebook.appendChild(p);
-    document.body.style.backgroundColor = `hsl(${Math.floor(Math.random() * 360)}, 100%, 20%)`;
+  // Listen to and process results
+  recognition.addEventListener('result', (e) => {
+    // First, convert array-like results to array
+    const transcript = Array.from(e.results)
+      // Store current speech
+      .map(result => result[0])
+      // Store transcript
+      .map(result => result.transcript)
+      // Combine both results into one string
+      .join('');
+
+    // Display transcript in notebook paragraph
+    p.textContent = transcript;
+
+    // At end of detected speech, create a new paragraph and change the background color
+    if (e.results[0].isFinal) {
+      p = document.createElement('p');
+      notebook.appendChild(p);
+      randomBgColor();
+    }
+
+    // Secret word to flash the background
+    if (transcript.includes('Magic')) {
+      document.body.classList.add('magic'); // 'magic' class triggers animation
+      setTimeout(() => document.body.classList.remove('magic'), 2000); // Remove class after animation completes
+    }
+  });
+
+  // At end of detected speech, begin recognition again
+  recognition.addEventListener('end', recognition.start);
+
+  // Begin speech recognition
+  recognition.start();
+}
+
+// Toggle speech recognition
+function toggleSpeech() {
+  // Start capturing speech and update UI
+  if (btn.textContent.includes('Begin')) {
+    captureSpeech();
+    randomBgColor();
+    btn.textContent = 'Click to Stop';
+  } else {
+    // Stop capturing speech and update UI
+    recognition.stop();
+    document.body.style.backgroundColor = 'hsl(60, 100%, 8%)';
+    recognition.removeEventListener('end', recognition.start);
+    btn.textContent = 'Click to Begin';
   }
-
-  // Secret word to flash the background
-  if (transcript.includes('Magic')) {
-    document.body.classList.add('magic'); // 'magic' class triggers animation
-    setTimeout(() => document.body.classList.remove('magic'), 2000); // Remove class after animation completes
-  }
-});
-
-// At end of detected speech, begin recognition again
-recognition.addEventListener('end', recognition.start);
-
-// Begin speech recognition
-recognition.start();
+}
