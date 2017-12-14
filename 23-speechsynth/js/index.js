99b54a1e303fcba03534392d307f3b27574c4b77
rvrvrv <ibanez19@sbcglobal.net>
2017-12-14 10:53:33 -0600

    Upload completed project files

---
+++ 23-speechsynth/js/index.js
@@ -0,0 +1,51 @@
+// Initialize Web Speech API
+const spokenMsg = new SpeechSynthesisUtterance();
+let voices = []; // Will store voices from Web Speech API
+// Store user's choices in options object
+const voiceSelect = document.getElementsByTagName('select')[0];
+const options = [
+  document.getElementsByName('rate')[0],
+  document.getElementsByName('pitch')[0],
+  document.getElementsByName('text')[0]
+];
+// Access speak and stop buttons
+const speakBtn = document.getElementById('speak');
+const stopBtn = document.getElementById('stop');
+// Store user's text in spokenMsg
+spokenMsg.text = options[2].value;
+
+// Retrieve and display available voices
+function populateVoices() {
+  // Retrieve voices from Web Speech API
+  voices = this.getVoices();
+  // Generate HTML for voiceSelect dropdown box
+  voiceSelect.innerHTML = voices
+    .map(v => `<option value="${v.name}">${v.name} (${v.lang})</option>`)
+    .join('');
+}
+
+// Stop speech, and optionally restart after brief delay
+function toggleSpeech(restart = true) {
+  speechSynthesis.cancel();
+  if (restart) setTimeout(() => speechSynthesis.speak(spokenMsg), 500);
+}
+
+// Update the user's voice selection
+function setVoice() {
+  // Set spokenMsg voice to dropdown value
+  spokenMsg.voice = voices.find(v => (v.name === this.value));
+  toggleSpeech();
+}
+
+// Update an option based on user's selection
+function setOption() {
+  spokenMsg[this.name] = this.value;
+  toggleSpeech();
+}
+
+// Event listeners
+speechSynthesis.addEventListener('voiceschanged', populateVoices);
+voiceSelect.addEventListener('change', setVoice);
+options.forEach(o => o.addEventListener('change', setOption));
+speakBtn.addEventListener('click', toggleSpeech);
+stopBtn.addEventListener('click', () => toggleSpeech(false));
