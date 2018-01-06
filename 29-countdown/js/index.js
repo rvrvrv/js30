e659f6639e348513e9bdfc98306be45f796cf172
rvrvrv <ibanez19@sbcglobal.net>
2018-01-06 15:16:31 -0600

    Complete timer functionality

--- 29-countdown/js/index.js
+++ 29-countdown/js/index.js
@@ -1,15 +1,27 @@
 let counter;
 const timeLeftDisplay = document.getElementsByClassName('display__time-left')[0];
 const endTimeDisplay = document.getElementsByClassName('display__end-time')[0];
+const buttons = document.querySelectorAll('button');
 
 function displayEndTime(timestamp) {
   // Calculate end time
   const end = new Date(timestamp);
   const hour = end.getHours();
   const min = end.getMinutes();
+  // Convert 24-hr to 12-hr (when applicable)
+  let convertedHour = false;
+  if (hour > 12) convertedHour = hour - 12;
+  else if (hour === 0) convertedHour = 12;
   // Format time
-  const formattedTime = `${hour > 12 ? hour - 12 : hour}:${min < 10 ? '0' : ''}${min} ${hour > 12 ? 'PM' : 'AM'}`;
-  endTimeDisplay.textContent = `Be back at ${formattedTime}`;
+  const formattedTime = `${convertedHour || hour}:${min < 10 ? '0' : ''}${min} ${hour > 12 ? 'PM' : 'AM'}`;
+  endTimeDisplay.textContent = `Done at ${formattedTime}`;
+}
+
+function stopTimer() {
+  // End counter
+  clearInterval(counter);
+  // Update UI
+  timeLeftDisplay.textContent = 'Done!';
 }
 
 function displayTimeLeft(sec) {
@@ -22,24 +34,45 @@ function displayTimeLeft(sec) {
   // Display time on page and in window title
   timeLeftDisplay.textContent = formattedTime;
   document.title = formattedTime;
-  // Display end time
-  displayEndTime(sec);
 }
 
 function timer(sec) {
+  // Clear running counter (when applicable)
+  clearInterval(counter);
   // Get current time and future time
   const now = Date.now();
   const then = now + (sec * 1000);
-  // Display initial time remaining
+  // Display initial time remaining and end time
   displayTimeLeft(sec);
+  displayEndTime(then);
   // Begin counter
   counter = setInterval(() => {
     // Get remaining time in seconds
     const remaining = Math.round((then - Date.now()) / 1000);
-    // When no time is left, stop the counter
-    if (remaining < 0) return clearInterval(counter);
     // Display updated time remaining
     displayTimeLeft(remaining);
+    // When no time is left, stop the timer
+    if (remaining < 1) stopTimer();
   }, 1000);
 }
 
+function startTimer() {
+  // Get number of seconds from data-time attribute
+  const seconds = parseInt(this.dataset.time, 10);
+  timer(seconds);
+}
+
+// Handle customForm submissions
+document.customForm.addEventListener('submit', (e) => {
+  // Don't refresh page
+  e.preventDefault();
+  // Store user input
+  const minutes = parseInt(document.customForm.minutes.value.trim(), 10);
+  // Validate input, convert minutes to seconds, and start timer
+  if (!isNaN(minutes) && minutes > 0) timer(minutes * 60);
+  else if (minutes === 0) stopTimer();
+  // If invalid input, clear the form
+  else document.customForm.minutes.value = '';
+});
+
+buttons.forEach(btn => btn.addEventListener('click', startTimer));
