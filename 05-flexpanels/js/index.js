13bbe59964c0e70dccfabd93b2ad247ca9fbc1fb
rvrvrv <richvcode@gmail.com>
2018-10-04 18:10:44 -0500

    Refactor and lint

--- 05-flexpanels/js/index.js
+++ 05-flexpanels/js/index.js
@@ -1,24 +1,18 @@
-/*jshint browser: true, esversion: 6*/
-/* global $*/
-
-
-//Store all panels in array
+// Store all panels in array
 const panels = document.querySelectorAll('.panel');
 
-//Toggle 'open' class
+// Toggle 'open' class
 	function toggleOpen() {
 		this.classList.toggle('open');
 	}
 
-//Toggle back to normal properties
+// Toggle back to normal properties
 function toggleActive(e) {
-	if (e.propertyName.includes('flex')) {
-		this.classList.toggle('open-active');
-	}
+	if (e.propertyName.includes('flex')) this.classList.toggle('open-active');
 }
 
-//Listen for clicks on each panel
+// After click, toggle 'open' class
 panels.forEach(panel => panel.addEventListener('click', toggleOpen));
-//Listen for end of transition on each panel
-panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
 
+// After transition, toggle 'active' class
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
