395b03f6971babaca29e3a7dc839d354371ccbfe
rvrvrv <richvcode@gmail.com>
2018-10-05 09:43:55 -0500

    Lint code

--- 06-typeahead/js/index.js
+++ 06-typeahead/js/index.js
@@ -1,17 +1,19 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   // Store location of JSON data
-  const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+  const endpoint =
+    "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json";
   const cities = [];
-  const searchInput = document.querySelector('.search');
-  const suggestions = document.querySelector('.suggestions');
+  const searchInput = document.querySelector(".search");
+  const suggestions = document.querySelector(".suggestions");
 
-  fetch(endpoint).then(resp => resp.json()) // Get JSON from response
+  fetch(endpoint)
+    .then(resp => resp.json()) // Get JSON from response
     .then(data => cities.push(...data)); // Store JSON data in cities array
 
   // Search for matching cities
   function findMatches(word) {
-    return cities.filter((place) => {
-      const regEx = new RegExp(word, 'gi');
+    return cities.filter(place => {
+      const regEx = new RegExp(word, "gi");
       return place.city.match(regEx) || place.state.match(regEx);
     });
   }
@@ -19,27 +21,37 @@ document.addEventListener('DOMContentLoaded', () => {
   // Display matching cities
   function displayMatches() {
     // Auto-capitalize user's entry
-    searchInput.style.textTransform = 'capitalize';
-    suggestions.style.textTransform = 'capitalize';
+    searchInput.style.textTransform = "capitalize";
+    suggestions.style.textTransform = "capitalize";
     // Store results in array
     const matchArr = findMatches(this.value);
     // Append results to suggestions list
-    const newHtml = matchArr.map((place) => {
-      // Find and highlight matching word
-      const reg2 = new RegExp(this.value, 'gi');
-      const hlCity = place.city.replace(reg2, `<span class="hl">${this.value}</span>`);
-      const hlState = place.state.replace(reg2, `<span class="hl">${this.value}</span>`);
-      // Format population with commas
-      const prettyPop = parseInt(place.population, 10).toLocaleString();
-      // Return formatted result
-      return `<li class="result">
-        <a href="http://www.google.com/search?q=${place.city},%20${place.state}" target=_blank>
+    const newHtml = matchArr
+      .map(place => {
+        // Find and highlight matching word
+        const reg2 = new RegExp(this.value, "gi");
+        const hlCity = place.city.replace(
+          reg2,
+          `<span class="hl">${this.value}</span>`
+        );
+        const hlState = place.state.replace(
+          reg2,
+          `<span class="hl">${this.value}</span>`
+        );
+        // Format population with commas
+        const prettyPop = parseInt(place.population, 10).toLocaleString();
+        // Return formatted result
+        return `<li class="result">
+        <a href="http://www.google.com/search?q=${place.city},%20${
+          place.state
+        }" target=_blank>
         <span class="name">${hlCity}, ${hlState}</span></a>
         <span class="population">${prettyPop}</span></li>`;
-    }).join('');
+      })
+      .join("");
     suggestions.innerHTML = newHtml;
   }
 
   // Listen for typing in search field
-  searchInput.addEventListener('keyup', displayMatches);
+  searchInput.addEventListener("keyup", displayMatches);
 });
