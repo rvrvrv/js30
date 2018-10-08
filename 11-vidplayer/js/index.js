72780c7af3423edc88c47a23a137959d0203059e
rvrvrv <richvcode@gmail.com>
2018-10-08 09:30:38 -0500

    Lint code

--- 11-vidplayer/js/index.js
+++ 11-vidplayer/js/index.js
@@ -1,73 +1,69 @@
-/*jshint browser: true, esversion: 6*/
-/* global $, alert, console */
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipBtns = player.querySelectorAll('[data-skip]');
+const fullScrnBtn = player.querySelector('#fullScrnBtn');
+const ranges = player.querySelectorAll('.player__slider');
 
-//Get all elements (instead of using jQuery)
-const player = document.querySelector(".player");
-const video = player.querySelector(".viewer");
-const progress = player.querySelector(".progress");
-const progressBar = player.querySelector(".progress__filled");
-const toggle = player.querySelector(".toggle");
-const skipBtns = player.querySelectorAll("[data-skip]");
-const fullScrnBtn = player.querySelector("#fullScrnBtn");
-const ranges = player.querySelectorAll(".player__slider");
-
-//Play or pause video
+// Play or pause video
 function togglePlay() {
-    video[video.paused ? "play" : "pause"]();
+  video[video.paused ? 'play' : 'pause']();
 }
 
-//Update play/pause button
+// Update play/pause button
 function updateBtn() {
-    let icon = this.paused ?
-        '<i class="fa fa-play"></i>' :
-        '<i class="fa fa-pause"></i>';
-    toggle.innerHTML = icon;
+  const icon = this.paused
+    ? '<i class="fa fa-play"></i>'
+    : '<i class="fa fa-pause"></i>';
+  toggle.innerHTML = icon;
 }
 
-//Skip video forward or backward
+// Skip video forward or backward
 function skip() {
-    video.currentTime += +this.dataset.skip;
+  video.currentTime += +this.dataset.skip;
 }
 
-//Handle volume and playback rate updates
+// Handle volume and playback rate updates
 function handleRangeUpdate() {
-    //this.name = corresponding video property
-    video[this.name] = this.value;
+  // this.name = corresponding video property
+  video[this.name] = this.value;
 }
 
-//Update progress bar
+// Update progress bar
 function updateProgress() {
-    const percent = video.currentTime / video.duration * 100;
-    progressBar.style.flexBasis = `${percent}%`;
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
 }
 
-//Scrub video
+// Scrub video
 function scrub(e) {
-    video.currentTime = e.offsetX / progress.offsetWidth * video.duration;
+  video.currentTime = e.offsetX / progress.offsetWidth * video.duration;
 }
 
-//Toggle full-screen video
+// Toggle full-screen video
 function toggleFullScreen() {
-    if (video.requestFullscreen) video.requestFullscreen();
-    else if (video.mozRequestFullScreen) video.mozRequestFullScreen();
-    else if (video.webkitRequestFullScreen) video.webkitRequestFullScreen();
-    else alert('Sorry, but full-screen video is not supported in your browser.');
+  if (video.requestFullscreen) video.requestFullscreen();
+  else if (video.mozRequestFullScreen) video.mozRequestFullScreen();
+  else if (video.webkitRequestFullScreen) video.webkitRequestFullScreen();
+  else alert('Sorry, but full-screen video is not supported in your browser.');
 }
 
-//Add event listeners
-video.addEventListener("click", togglePlay);
-video.addEventListener("play", updateBtn);
-video.addEventListener("pause", updateBtn);
-video.addEventListener("timeupdate", updateProgress);
-toggle.addEventListener("click", togglePlay);
-fullScrnBtn.addEventListener("click", toggleFullScreen);
+// Add event listeners
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateBtn);
+video.addEventListener('pause', updateBtn);
+video.addEventListener('timeupdate', updateProgress);
+toggle.addEventListener('click', togglePlay);
+fullScrnBtn.addEventListener('click', toggleFullScreen);
 
-skipBtns.forEach(btn => btn.addEventListener("click", skip));
-ranges.forEach(range => range.addEventListener("change", handleRangeUpdate));
+skipBtns.forEach(btn => btn.addEventListener('click', skip));
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
 
-//Progress bar scrubbing
+// Progress bar scrubbing
 let mousedown = false;
-progress.addEventListener("click", scrub);
-progress.addEventListener("mousemove", e => mousedown && scrub(e));
-progress.addEventListener("mousedown", () => (mousedown = true));
-progress.addEventListener("mouseup", () => (mousedown = false));
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', e => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => (mousedown = true));
+progress.addEventListener('mouseup', () => (mousedown = false));
