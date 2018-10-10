a6fbf9b28baf5b5ebbe7a22af6d1918ccc8a88c3
rvrvrv <richvcode@gmail.com>
2018-10-10 10:16:00 -0500

    Adjust comments and lint

--- 14-refvscopy/refvscopy.js
+++ 14-refvscopy/refvscopy.js
@@ -1,25 +1,20 @@
-/*jshint browser: true, esversion: 6*/
-/* global console */
-
-//Arrays
+/* ARRAYS */
 
 const players = ['Wes', 'Sarah', 'Ryan', 'Poppy'];
 
-//Copy methods (new array, not reference to original)
+// Copy methods (new array, not reference to original)
 
-//Slice
+// Slice
 const team2 = players.slice();
 
-
-//Concat
+// Concat
 const team3 = [].concat(players);
 
-//ES6 spread
+// ES6 spread
 const team4 = [...players];
 
-//Array.from
+// Array.from
 const team5 = Array.from(players);
-
 
 console.log('players:', players);
 console.log('team2:', team2);
@@ -27,23 +22,21 @@ console.log('team3:', team3);
 console.log('team4:', team4);
 console.log('team5:', team5);
 
-
-//Objects
+/* OBJECTS */
 
 const person = {
-	name: 'Rich V.',
-	age: 29,
-	social: {
-		github: 'rvrvrv'
-	}
+  name: 'Rich V.',
+  age: 29,
+  social: {
+    github: 'rvrvrv'
+  }
 };
 
-//Copy method (new object, not reference to original)
+// Copy method (new object, not reference to original)
 
-//Object.assign
+// Object.assign
 const person2 = Object.assign({}, person);
-//^^Only one level deep. Nested person2 changes also affect person.
-
+// ^^Only one level deep. Changes to person2 also affect person.
 
 console.log('person:', person);
 console.log('person2:', person2);
